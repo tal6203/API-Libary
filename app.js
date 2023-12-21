5a61e2a2fe1223cae989d165fa68957f818f2815
Tal Abutbul <112417918+tal6203@users.noreply.github.com>
2023-12-21 15:59:39 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const app = express();
+const routes = require('./routes/routes');
 const path = require('path');
-const port = process.env.PORT || 8080;
+const port = 8080;
 
-// Serve static files from the 'static' and 'script' directories
-app.use(express.static(path.join(__dirname, 'static')));
-app.use(express.static(path.join(__dirname, 'script')));
+app.use(express.static(path.join('.', '/static/')));
 
-// Fallback route to serve 'search_book.html' for any other route
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'static', 'search_book.html'));
-});
+app.use(express.static(path.join('.', '/script/')));
+
+app.use(routes);
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
